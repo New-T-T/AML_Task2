886eae9174377988c86ec7f9bdc79d1b62d8b80a
Maja Gwóźdź <maja.k.gwozdz@gmail.com>
2022-11-28 04:54:43 +0100

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -5,6 +5,10 @@ import biosppy.signals.ecg as ecg
 import neurokit2 as nk
 import heartpy as hp
 
+import numpy as np
+
+import scipy.stats as stats
+
 # Load datasets, decompressing pickle with gzip
 X_train = pd.read_pickle('data/X_train.pkl', compression='gzip')
 y_train = pd.read_pickle('data/y_train.pkl', compression='gzip')
@@ -16,6 +20,10 @@ X_test = pd.read_pickle('data/X_test.pkl', compression='gzip')
     #Feature 1: number of spikes/total peaks
     #Feature 2: STD (after modified zscore)
     #Feature 3: MAD (after modified zscore)
+    #Feature 4: peak median
+    #Feature 5: peak mean
+    #Feature 6: min peak
+    #Feature 7: max peak
 
 # modified zscore based on MAD (median absolute deviation)
 def modified_zscore(data, consistency_correction = 1.4826):
@@ -29,7 +37,8 @@ def modified_zscore(data, consistency_correction = 1.4826):
 
 def feat_rpeak_variance(data_ts, static_rpeak_threshold = 400):           #returns an array of the features
 
-    feature_rpeaks_variance = np.empty(3)
+    feature_rpeaks_variance = np.empty(7)
+
     for i in range(len(data_ts)):
 
         #capture the signal (clean and use neurokit.ecg_findpeaks)
@@ -40,9 +49,18 @@ def feat_rpeak_variance(data_ts, static_rpeak_threshold = 400):           #retur
         #extract the values of the peaks
         rpeak_values_cleaned = cleaned_signal[r_peaks_cleaned]
 
-        #median & median
+        #median & mean
         median_rpeaks = np.median(rpeak_values_cleaned)
         mean_rpeak_cleaned = np.mean(rpeak_values_cleaned)
+
+        #max & min
+        try:
+            max_rpeaks = np.max(rpeak_values_cleaned)
+            min_rpeaks = np.min(rpeak_values_cleaned)
+
+        except ValueError:
+            max_rpeaks = np.nan
+            min_rpeaks = np.nan
 
         # (modified)-zscore for rpeak-outlier detection
 
@@ -60,7 +78,7 @@ def feat_rpeak_variance(data_ts, static_rpeak_threshold = 400):           #retur
         std_rpeaks = np.std(rpeak_values_cleaned)
         mad_rpeaks = stats.median_abs_deviation(rpeak_values_cleaned)
 
-        #detect spikes (i.e. unusual high R_peaks)
+        #detect spikes (i.e., unusually high R_peaks)
         counter_spikes = 0
         for j in r_peaks_cleaned:
             if abs(cleaned_signal[j]) > abs((median_rpeaks + static_rpeak_threshold)):
@@ -70,7 +88,7 @@ def feat_rpeak_variance(data_ts, static_rpeak_threshold = 400):           #retur
             counter_spikes /= len(r_peaks_cleaned)                            #normalize the spikes with respect to the total number of R_peaks
 
         #add features
-        var_feat = [counter_spikes, std_rpeaks, mad_rpeaks]
+        var_feat = [counter_spikes, std_rpeaks, mad_rpeaks, median_rpeaks, median_rpeaks, max_rpeaks, min_rpeaks]
         feature_rpeaks_variance = np.vstack([feature_rpeaks_variance, var_feat])
 
     feature_rpeaks_variance = np.delete(feature_rpeaks_variance, 0, 0)        #delete the (random) first column
@@ -79,12 +97,118 @@ def feat_rpeak_variance(data_ts, static_rpeak_threshold = 400):           #retur
 
     return feature_rpeaks_variance
 
+#Q,S,T-peaks
+#18 features in total
+#For each peak type:
+    #Feature1: mean
+    #Feature2: median
+    #Feature3: max
+    #Feature4: min
+    #Feature5: STD
+    #Feature6: MAD
+
+def QST_peaks(data_ts):
+
+    feature_qst_peaks = np.empty(18)
+
+    counter = 0 # for counting the skipped iterations
+
+    for i in range(len(data_ts)):
+
+        print(f"Iteration {i}")
+
+        try:
+            #capture the signal and clean it
+            raw_signal = data_ts[i,][~np.isnan(data_ts[i,])]
+
+            #print(len(raw_signal))
+
+            # delineate the cleaned ECG signal to get the Q,S,T-peaks
+            cleaned_signal = nk.ecg_clean(raw_signal, sampling_rate=300, method="neurokit")
+            _, rpeaks = nk.ecg_peaks(cleaned_signal, sampling_rate=300)
+            signals, waves = nk.ecg_delineate(cleaned_signal, rpeaks, sampling_rate=300, method="dwt")
+
+            # Q-peak features
+            qpeaks_mean = np.mean(waves["ECG_Q_Peaks"])
+            qpeaks_median = np.median(waves["ECG_Q_Peaks"])
+            qpeaks_max = np.max(waves["ECG_Q_Peaks"])
+            qpeaks_min = np.min(waves["ECG_Q_Peaks"])
+            qpeaks_std = np.std(waves["ECG_Q_Peaks"])
+            qpeaks_mad = stats.median_abs_deviation(waves["ECG_Q_Peaks"])
+
+        except:
+            #print(f"No result for {i}.")
+            counter += 1
+            qpeaks_mean = np.nan
+            qpeaks_median = np.nan
+            qpeaks_max = np.nan
+            qpeaks_min = np.nan
+            qpeaks_std = np.nan
+            qpeaks_mad = np.nan
+
+        try:
+            # S-peak features
+            speaks_mean = np.mean(waves["ECG_S_Peaks"])
+            speaks_median = np.median(waves["ECG_S_Peaks"])
+            speaks_max = np.max(waves["ECG_S_Peaks"])
+            speaks_min = np.min(waves["ECG_S_Peaks"])
+            speaks_std = np.std(waves["ECG_S_Peaks"])
+            speaks_mad = stats.median_abs_deviation(waves["ECG_S_Peaks"])
+
+        except:
+            #print(f"No result for {i}.")
+            counter += 1
+            speaks_mean = np.nan
+            speaks_median = np.nan
+            speaks_max = np.nan
+            speaks_min = np.nan
+            speaks_std = np.nan
+            speaks_mad = np.nan
+
+        try:
+            # T-peak features
+            tpeaks_mean = np.mean(waves["ECG_T_Peaks"])
+            tpeaks_median = np.median(waves["ECG_T_Peaks"])
+            tpeaks_max = np.max(waves["ECG_T_Peaks"])
+            tpeaks_min = np.min(waves["ECG_T_Peaks"])
+            tpeaks_std = np.std(waves["ECG_T_Peaks"])
+            tpeaks_mad = stats.median_abs_deviation(waves["ECG_T_Peaks"])
+
+        except:
+            #print(f"No result for {i}.")
+            counter += 1
+            tpeaks_mean = np.nan
+            tpeaks_median = np.nan
+            tpeaks_max = np.nan
+            tpeaks_min = np.nan
+            tpeaks_std = np.nan
+            tpeaks_mad = np.nan
+
+        #add features
+        qst_feat = [qpeaks_mean, qpeaks_median, qpeaks_max, qpeaks_min, qpeaks_std, qpeaks_mad,
+            speaks_mean, speaks_median, speaks_max, speaks_min, speaks_std, speaks_mad,
+            tpeaks_mean, tpeaks_median, tpeaks_max, tpeaks_min, tpeaks_std, tpeaks_mad]
+
+        feature_qst_peaks = np.vstack([feature_qst_peaks, qst_feat])
+
+    print(f"Counter is: {counter}.")
+
+    feature_qst_peaks = np.delete(feature_qst_peaks, 0, 0)        #delete the (random) first column
+
+    return feature_qst_peaks
+
 
 # HRV: heart rate variability
         #Feature 1: median_hr (bio-rpeaks)
         #Feature 2: median_hr_cleaned (nk-rpeaks)
         #Feature 3: hrv (=STD after zscore of bio-rpeaks)
         #Feature 4: hrv_cleaned (=STD after zscore of nk-rpeaks)
+        #Feature 5: mean hr (bio-rpeaks)
+        #Feature 6: mean hr_cleaned (nk-rpeaks)
+        #Feature 7: min hr (bio-rpeaks)
+        #Feature 8: min hr_cleaned (nk-rpeaks)
+        #Feature 9: max hr (bio-rpeaks)
+        #Feature10: max hr_cleaned (nk-rpeaks)
 
 def feat_hrv(data_ts):
 
@@ -108,7 +232,7 @@ def feat_hrv(data_ts):
 
 
     #extract properties/features (mean)
-    feature_hrv = np.empty(4)
+    feature_hrv = np.empty(10)
 
     for i in feature_hr:
 
@@ -118,6 +242,23 @@ def feat_hrv(data_ts):
 
         mean_hr = np.mean(i[0])
         mean_hr_cleaned = np.mean(i[1])
+
+        # min & max
+        try:
+            min_hr = np.min(i[0])
+            min_hr_cleaned = np.min(i[1])
+
+        except ValueError:
+            min_hr = np.nan
+            min_hr_cleaned = np.nan
+
+        try:
+            max_hr = np.max(i[0])
+            max_hr_cleaned = np.max(i[1])
+
+        except ValueError:
+            max_hr = np.nan
+            max_hr_cleaned = np.nan
 
         z_threshold = 3
 
@@ -136,9 +277,10 @@ def feat_hrv(data_ts):
         median_hr = np.median(i[0])
         median_hr_cleaned = np.median(i[1])
 
-
         #add features
-        hrv_feat = [median_hr, median_hr_cleaned, hrv, hrv_cleaned]
+        hrv_feat = [median_hr, median_hr_cleaned, hrv, hrv_cleaned,
+            mean_hr, mean_hr_cleaned, min_hr, min_hr_cleaned, max_hr, max_hr_cleaned]
+
         feature_hrv = np.vstack([feature_hrv, hrv_feat])
 
     feature_hrv = np.delete(feature_hrv, 0, 0)
@@ -160,8 +302,22 @@ def feature_extraction(train_data_ts, test_data_ts):
     feat_rpeak_var_test = feat_rpeak_variance(test_data_ts)
     print("finished with R_peak_variance-features")
 
-    #Stack the features
-    X_train_features = np.c_[ feat_hrv_train, feat_rpeak_var_train ]
-    X_test_features = np.c_[ feat_hrv_test, feat_rpeak_var_test ]
+    #QST_peaks
+    feat_qst_peak_train = QST_peaks(train_data_ts)
+    feat_qst_peak_test = QST_peaks(test_data_ts)
+    print("finished with QST_peak-features")
 
-    return X_train_features, X_test_features
+    #Stack the features
+    X_train_features = np.c_[ feat_hrv_train, feat_rpeak_var_train, feat_qst_peak_train ]
+    X_test_features = np.c_[ feat_hrv_test, feat_rpeak_var_test, feat_qst_peak_test ]
+
+    print("Feature extraction complete.")
+
+    return pd.DataFrame.from_records(X_train_features), pd.DataFrame.from_records(X_test_features)
+
+print(f"X_train: {X_train.shape} and X_test: {X_test.shape}.")
+
+X_train_features, X_test_features = feature_extraction(X_train.to_numpy(), X_test.to_numpy())
+
+print(X_train_features.to_string())
+print(X_test_features.to_string())
